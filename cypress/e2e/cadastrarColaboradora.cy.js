c13e4ccbc6af4dd0e16220c4f3af71b512776964
CelioJr12 <celiojunio878@gmail.com>
2024-11-27 19:16:52 -0300

    cypress

--- cypress/e2e/cadastrarColaboradora.cy.js
+++ cypress/e2e/cadastrarColaboradora.cy.js
@@ -1,13 +1,31 @@
 describe('Cadastrando colaboradora', () => {
     it('cenario1', () => {
-        //steps do cenario1
+        cy.visit('/');
+        cy.get('[href="/login/"] > h1').click();
+        cy.get('#username').type('cypress');
+        cy.get('#password').type('abc123');
+        cy.get('.button').click();
+        cy.get('[href="/aplicacao/home_cadastros/"] > .card').click();
+        cy.get('.novo-cadastro-card').click()
     })
 
     it('cenario2', () => {
-        //steps do cenario2
+        cy.visit('/');
+        cy.get('[href="/login/"] > h1').click();
+        cy.get('#username').type('cypress');
+        cy.get('#password').type('abc123');
+        cy.get('.button').click();
+        cy.get('[href="/aplicacao/home_cadastros/"] > .card').click();
+        cy.get('.novo-cadastro-card').click()
     })
 
     it('cenario3', () => {
-        //steps do cenario3
+        cy.visit('/');
+        cy.get('[href="/login/"] > h1').click();
+        cy.get('#username').type('cypress');
+        cy.get('#password').type('abc123');
+        cy.get('.button').click();
+        cy.get('[href="/aplicacao/home_cadastros/"] > .card').click();
+        cy.get('.novo-cadastro-card').click()
     })
 })
